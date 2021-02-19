6f2e1971d21d547488b8b6349b433ed18c7b9f17
dasom <dasom228@gmail.com>
2021-02-19 18:05:56 +0900

    class interface and genericType study

--- dist/practice.js
+++ dist/practice.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var message = 'hello world';
 var messages = [];
 var numbers = [];
@@ -15,4 +26,27 @@ var nothingReturn = function () {
     console.log('return값 없는 함수로 결과값 타입 void로 설정 했는데 return 하려고 하면 오류남');
     // return 4
 };
-var numberOrString = function () { return 'string'; };
+var numberOrString = function () { return 0; };
+var Retangle = /** @class */ (function () {
+    function Retangle(width, height) {
+        this.width = width;
+        this.height = height;
+        this.width = width;
+        this.height = height;
+    }
+    Retangle.prototype.getArea = function () {
+        return this.width * this.height;
+    };
+    return Retangle;
+}());
+var retangle = new Retangle(100, 100);
+console.log('retangle', retangle.getArea());
+// generic
+function merge(a, b) {
+    return __assign(__assign({}, a), b);
+}
+function merge2(a, b) {
+    return __assign(__assign({}, a), b);
+}
+var merged = merge2({ id: 1, content: 'foo' }, { id: 2, content: 'bar' });
+console.log('merged', merged);
--- src/practice.ts
+++ src/practice.ts
@@ -22,4 +22,50 @@ const nothingReturn = (): void => {
   // return 4
 }
 
-const numberOrString = ():number | string =>  'string'
+const numberOrString = ():number | string =>  0
+
+// interface
+interface Shape {
+  getArea(): number // return값은 number
+}
+
+class Retangle implements Shape {
+  constructor(private width: number, private height: number) { // public이나, private 키워드를 넣어주면 this.width에 할당해주는 작업 생략 가능
+    // this.width = width
+    // this.height = height
+  }
+  getArea() {
+    return this.width * this.height
+  }
+}
+
+const retangle = new Retangle(100, 100)
+console.log('retangle', retangle.getArea())
+
+// generic (any와 동일한 효과지만 generic으로 사용하면 해당 값이 할당된곳에서 사용된 타입을 알수있음)
+
+function merge (a: object, b: object) { //이렇게 사용하면 merged안의 객체속성을 모름
+  return {
+    ...a,
+    ...b
+  }
+}
+
+function merge2<T1, T2> (a: T1, b: T2) { //이렇게 사용하면 merged안의 객체속성을 모름
+  return {
+    ...a,
+    ...b
+  }
+}
+
+
+const merged = merge2({id: 1, content: 'foo'}, {id: 2, content: 'bar'})
+console.log('merged', merged)
+
+function wrap<T> (params: T) {
+  return {
+    params
+  }
+}
+
+const wrapped = wrap(10)
